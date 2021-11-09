ca49a4a6d5005fffb3a18a8a9e3c0c8dd7d0c31c
Mohed <mccdama@gmail.com>
2021-11-09 10:19:15 +0100

    const.go

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Println("go" + "lang")
+	fmt.Println("17 + 37 =", 17+37)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
