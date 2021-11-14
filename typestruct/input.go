64dcb3ffe068e60a1787178ade8a1a2e6e242dae
Mohed <mccdama@gmail.com>
2021-11-14 01:21:25 +0100

    Switch btw. user inputs

--- typestruct/input.go
+++ typestruct/input.go
@@ -17,6 +17,25 @@ func GetInput(prompt string, r *bufio.Reader) (string, error) {
 func PromptOpt(b Bill) {
 	reader := bufio.NewReader(os.Stdin)
 
-	opt, _ := GetInput("Choose option (a - add item, s - save bill, t - add tip):", reader)
-	fmt.Print(opt)
+	opt, _ := GetInput("Choose option (a - add item, s - save bill, t - add tip, q - for exit):", reader)
+	// fmt.Print(opt)
+
+	switch opt {
+	case "a":
+		name, _ := GetInput("Item name: ", reader)
+		price, _ := GetInput("Item price: ", reader) // we got string from input!--> turn into float
+		fmt.Println(name, price)
+
+	case "s":
+		fmt.Println("you chose s")
+	case "t":
+		tip, _ := GetInput("Tip amount ($): ", reader)
+		fmt.Println(tip)
+
+	case "q":
+		break
+	default:
+		fmt.Println("Please enter a valid letter... ")
+		PromptOpt(b) // recycle the func
+	}
 }
