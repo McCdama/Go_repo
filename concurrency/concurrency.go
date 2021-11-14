fa77a99d3c8286afaf445d76a429ab1bb257d447
Mohed <mccdama@gmail.com>
2021-11-14 23:35:02 +0100

    URL result in channel

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,6 +1,10 @@
 package concurrency
 
-import "time"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 var URLs = []string{
 	"https://gitlab.com/",
@@ -9,10 +13,40 @@ var URLs = []string{
 	"https://www.francemusique.fr/",
 }
 
-type result struct {
-	url     string
-	elapsed time.Duration
-	status  string
-	size    int
-	err     error
+type Result struct {
+	Url     string
+	Elapsed time.Duration
+	Status  string
+	Size    int
+	Err     error
+}
+
+func Run() {
+	ch := make(chan Result)
+
+	// fire one goroutine per URL
+	for _, Url := range URLs {
+		go Get(ch, Url)
+	}
+}
+
+func Get(ch chan Result, url string) {
+	start := time.Now()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- Result{Err: err}
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		ch <- Result{
+			Url:     url,
+			Elapsed: time.Since(start),
+			Err:     fmt.Errorf("HTTP status code %d", resp.StatusCode),
+			Status:  resp.Status,
+		}
+		return
+	}
 }
