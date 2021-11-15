bc6d2cb9d8b53d833d4a53406955c3fbde8187e6
Mohed-Ubuntu <mccdama@gmail.com>
2021-11-15 22:34:13 +0100

    Add Match as a goroutine

--- feeds_app/search/match.go
+++ feeds_app/search/match.go
@@ -1,5 +1,7 @@
 package search
 
+import "log"
+
 // Result contains the result of a search
 type Result struct {
 	Field   string
@@ -9,4 +11,19 @@ type Result struct {
 // Matcher defiens the behavior required by types that want to implement a new search type
 type Matcher interface {
 	Search(feed *Feed, searchTerm string) ([]*Result, error)
+}
+
+// Match is launched as a goroutine for each individual feed to run search concurrently
+func Match(matcher Matcher, feed *Feed, searchTerm string, results chan<- *Result) {
+	// Perform the search against the specified matcher
+	searchResult, err := matcher.Search(feed, searchTerm)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Write the results to the channels
+	for _, result := range searchResult {
+		results <- result
+	}
 }
