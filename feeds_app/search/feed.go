0c77f09c69e4a4e964b00a504218c036c93bb6fc
Mohed-Ubuntu <mccdama@gmail.com>
2021-11-15 20:02:20 +0100

    implement feed class to read and decode JSON

--- feeds_app/search/feed.go
+++ feeds_app/search/feed.go
@@ -1,1 +1,31 @@
 package search
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const dataFile = "data/data.json"
+
+type Feed struct {
+	Name string `json:"site"`
+	URI  string `json:link"`
+	Type string `json:type"`
+}
+
+// RetrieveFeeds reads and unmarshlas the feed data file
+func RetrieveFeeds() ([]*Feed, error) {
+	file, err := os.Open(dataFile)
+	if err != nil {
+		return nil, err
+	}
+
+	// Schedule the file to be closed once the func returns
+	defer file.Close()
+
+	// Decode the file into a slice of pointers to Feed values
+	var feeds []*Feed
+	err = json.NewDecoder(file).Decode(&feeds)
+
+	return feeds, err
+}
