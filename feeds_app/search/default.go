c682896f79017b6a463ebb5eeed707bc783a4be4
Mohed-Ubuntu <mccdama@gmail.com>
2021-11-15 20:37:16 +0100

    Impl. of default matcher

--- feeds_app/search/default.go
+++ feeds_app/search/default.go
@@ -1,1 +1,14 @@
 package search
+
+type defaultMatcher struct{}
+
+// init registers the def. mat. with the prog.
+func init() {
+	var matcher defaultMatcher
+	Register("default", matcher)
+}
+
+// Search implements the behavior for the def. mat.
+func (m defaultMatcher) Search(feed *Feed, searchTerm string) ([]*Result, error) {
+	return nil, nil
+}
