b0a934bc913f2c38f0ab19f4ba842c9894b10d64
Mohed-Ubuntu <mccdama@gmail.com>
2021-11-15 20:11:51 +0100

    matcher struct and interface

--- feeds_app/search/match.go
+++ feeds_app/search/match.go
@@ -1,1 +1,12 @@
 package search
+
+// Result contains the result of a search
+type Result struct {
+	Field   string
+	Content string
+}
+
+// Matcher defiens the behavior required by types that want to implement a new search type
+type Matcher interface {
+	Search(feed *Feed, searchTerm string) ([]*Result, error)
+}
