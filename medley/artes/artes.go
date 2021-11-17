7fc32f184aa69b6f3c0287a619a5e73895d77887
Mohed <mccdama@gmail.com>
2021-11-17 23:17:18 +0100

    Going further with arrays

--- medley/artes/artes.go
+++ medley/artes/artes.go
@@ -1,8 +1,8 @@
-package artes
+package main
 
 import "fmt"
 
-func Main_array() {
+func main() {
 	var a [5]int
 	fmt.Println("Empty array: ", a)
 	fmt.Println("Array's length: ", len(a))
@@ -24,4 +24,35 @@ func Main_array() {
 		}
 	}
 	fmt.Println("Two Dimensional Array", twoD)
+
+	array_1 := [5]int{10, 20, 30, 40, 50}
+	fmt.Println("Array_1", array_1)
+
+	array_2 := [...]int{10, 20, 30, 40, 50}
+	fmt.Println("Array_2", array_2)
+
+	array_3 := [5]int{1: 10, 2: 20}
+	fmt.Println("Array_3", array_3)
+
+	array_2[2] = 35
+	fmt.Println("Array_2", array_2)
+
+	// Declare an integer pointer array of five elements.
+	// Initialize index 0 and 1 of the array with integer pointers.
+	array_4 := [5]*int{0: new(int), 1: new(int)}
+	// Assign values to index 0 and 1.
+	*array_4[0] = 10
+	*array_4[1] = 20
+	fmt.Println("Array_pointer", array_4)
+
+	// Declare a string array of five elements.
+	var array_5_0 [5]string
+	fmt.Println("Array_5_0", array_5_0)
+	// Declare a second string array of five elements.
+	// Initialize the array with colors.
+	array_5_1 := [5]string{"Red", "Blue", "Green", "Yellow", "Pink"}
+	fmt.Println("Array_5_1", array_5_1)
+	// Copy the values from array2 into array1.
+	array_5_0 = array_5_1
+	fmt.Println("Array_5_0", array_5_0)
 }
